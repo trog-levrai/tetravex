20450087bbf56884793b6606593f5237bd53c4f6
Ilan Dubois <ilan.dubois@epita.fr>
2018-01-09 21:55:49 +0100

    Add tetra object

--- src/main.cc
+++ src/main.cc
@@ -1,5 +1,9 @@
 #include <iostream>
 
-int main() {
+#include "tetra.hh"
+
+int main(int argc, char** argv) {
   std::cout << "Hello World!" << std::endl;
+  for (int i = 0; i < argc; ++i)
+    std::cout << argv[i] << std::endl;
 }
---
+++ src/tetra.hh
@@ -0,0 +1,10 @@
+class Tetra {
+  public:
+    Tetra(short u, short r, short d, short l)
+    : up(u)
+    , right(r)
+    , down(d)
+    , left(l)
+    {};
+    short up, right, down, left;
+};
